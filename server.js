3e245027f67e70448eb33fba5f0e675ceb44ad44
lopezsarmiento <diego.lopezsarmiento@gmail.com>
2020-10-02 17:25:30 -0300

    adding cors for https support

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 "use strict";
 // Dependencies
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./startup/db");
 const todos = require("./routes/todos");
 const home = require("./routes/home");
 const app = express();
+
+// local variables
 const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 //Routes
 app.use("/api", todos);
 app.use("/", home);
 
+// start db
 connectDB();
 
 // start server
